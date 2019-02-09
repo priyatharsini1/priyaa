4541312a3e6ff290a239eb316bf3a2fe356639e0
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-09 15:53:24 +0530

    Create set1.3.c

---
+++ set1.3.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main()
+{
+ char a;
+ scanf("%c",&a);
+ if((a=='a')||(a=='e')||(a=='i')||(a=='o')||(a=='u'))
+  {printf("vowels");
+  }
+  else
+  {
+      printf("consonents");
+  }
+  
+  return 0;
+}
