576d67c911a55170dcb44b8bfad28e955863f5ad
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-10 22:15:47 +0530

    Create set71.c

---
+++ set71.c
@@ -0,0 +1,20 @@
+#include<stdio.h>
+#include<conio.h>
+#include<string.h>
+void main()
+{
+char a[30],b[20];
+clrscr();
+gets(a);
+strcpy(b,a);
+strrev(b);
+if(strcmp(a,b)==0)
+{
+printf("yes");
+}
+else
+{
+printf("no");
+}
+getch();
+}
