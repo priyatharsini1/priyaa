2b7618e0d2a935c836b37e84ceb067ee708bcb66
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-10 22:18:07 +0530

    Create set74.c

---
+++ set74.c
@@ -0,0 +1,23 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+float n,k=0.1;
+int a,i=0;
+clrscr();
+scanf("%f",&n);
+for(i=0;i<9;i++)
+{
+a=(int)(n+k);
+if(a>n)
+{
+printf("%d",a);
+break;
+}
+else
+{
+k=k+0.1;
+}
+}
+getch();
+}
