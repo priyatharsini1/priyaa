295a52dc10a5c7d74a9c56cc3ceb13cc2cc81947
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-10 16:33:23 +0530

    Create set1.6.c

---
+++ set1.6.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main()
+{
+    int n;
+    scanf("%d",&n);
+    if((n%4==0)&&(n%400==0)&&(n%100==0))
+    {
+        printf("leap year");
+        
+    }
+    else
+    {
+        printf("not a leapyear");
+    }
+    return 0;
+}
