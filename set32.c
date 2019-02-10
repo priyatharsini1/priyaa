e54e28b9d3600264b4ef42f20c0065106016e6f9
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-10 17:32:47 +0530

    Create set32.c

---
+++ set32.c
@@ -0,0 +1,18 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+char a[20];
+int i,count=1;
+clrscr();
+gets(a);
+for(i=0;i<n;i++)
+{
+if(a[i]==' ')
+{
+count++;
+}
+}
+printf("%d",count);
+getch();
+}
