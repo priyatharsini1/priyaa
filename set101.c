2e1cc70a82feeb89eba8e73fa8d143cce6134e6e
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-10 21:58:37 +0530

    Create set101.c

---
+++ set101.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+#include<conio.h>
+int main(void)
+{
+  char str[50];
+	int i,n;
+	scanf("%s %d",str,&n);
+	int len;
+	len=strlen(str);
+	for(i=n;i<=len;i++)
+	{
+		printf("%c",str[i]);
+	}
+getch();
+}
