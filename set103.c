3d601f3e703bdc068edfb687f0c421363075f86f
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-11 19:24:48 +0530

    Update set103.c

--- set103.c
+++ set103.c
@@ -4,7 +4,7 @@ int main(void)
  {
 	char str[50];
 	int i=0,n;
-	scanf("%[^\n]s",str);
+	scanf(" %[^\n]s",str);
 	n=strlen(str);
   for(i=0;str[i]!='\0';i++)
 	{
@@ -13,6 +13,6 @@ int main(void)
 		if(str[i]==' ')
 			str[i+1]=toupper(str[i+1]);
 	}
-	printf("\n%s",str);
+	printf(" \n%s",str);
 	getch();
 }
