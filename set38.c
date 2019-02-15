6f3dda89527c5119321c408cdafee81af68d1d7f
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-15 15:32:54 +0530

    Update set38.c

--- set38.c
+++ set38.c
@@ -5,9 +5,9 @@ void main()
 {
 int num1,num2;
 clrscr();
-scanf("%d%d",&num1,&num2);
+scanf("%d %d",&num1,&num2);
 swap(&num1,&num2);
-printf("%d %d",num1,num2);
+printf("%d  %d",num1,num2);
 getch();
 }
 void swap(int *x,int *y)
