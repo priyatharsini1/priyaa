d8644203fe1bd3b11c9a3551a92a634c976d246f
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-19 07:35:51 -0800

    Create 44.c

---
+++ 44.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+void main() 
+{
+   int a,b,c,k=0,temp,d;
+   scanf("%d",&c);
+   if(c>=1&&c<=10)
+   {
+       printf("Yes");
+   }
+   else
+   {
+       printf("No");
+   }
+   getch();
+}
