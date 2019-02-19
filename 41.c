7e8b0e22e47e8b6e1a80d7acecb87a038522a01e
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-19 07:33:53 -0800

    Create 41.c

---
+++ 41.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void main() 
+{
+   int a,b=0,c=1,k=0,tot=0,i;
+   char z[50];
+    scanf("%d",&a);
+    scanf("%s",z);
+    for(i=0;i<a;i++)
+    {
+    printf("%s ",z);
+    }
+   getch();
+   
+}
