812748b370ccefba92f69ae0858096770160ff60
priyatharsini1 <46959639+priyatharsini1@users.noreply.github.com>
2019-02-20 15:19:16 +0530

    Update indexval.c

--- indexval.c
+++ indexval.c
@@ -4,14 +4,14 @@ void main()
 {
 int a[20],i,n;
 clrscr();
-scanf("%d",&n);
+scanf("   %d",&n);
 for(i=0;i<n;i++)
 {
 scanf("%d",&a[i]);
 }
 for(i=0;i<n;i++)
 {
-printf("%d %d",a[i],i);
+printf("%d   %d",a[i],i);
 printf("\n");
 }
 getch();
